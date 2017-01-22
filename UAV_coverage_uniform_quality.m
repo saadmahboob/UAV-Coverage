2c4254f65d451e6234cfab4387141fc969efa984
sotpapathe <sotpapathe@google.com>
2017-01-22 19:28:04 +0100

    Created control law function

--- UAV_coverage_uniform_quality.m
+++ UAV_coverage_uniform_quality.m
@@ -43,6 +43,10 @@ axy = 1;
 % Altitude control law gain
 az = 1;
 
+% Point importance function
+phi = @PHI_uniform;
+% phi = @PHI_gaussian1;
+
 % Network plots to show during simulation
 PLOT_STATE_3D = 0;
 PLOT_STATE_2D = 1;
@@ -73,14 +77,14 @@ axis_scale = [-0.5 3 -0.5 3];
 
 % ---------------- Initial State ----------------
 % Initial positions - 3 nodes - 15 seconds
-% X = [0.40, 0.60, 0.55];
-% Y = [0.50, 0.60, 0.50];
-% Z = [0.45, 0.55, 0.50];
+X = [0.40, 0.60, 0.55];
+Y = [0.50, 0.60, 0.50];
+Z = [0.45, 0.55, 0.50];
 
 % Initial positions - 9 nodes - 20 seconds
-X = [0.40, 0.60, 0.55, 0.60, 0.50, 0.70, 0.60, 0.90, 0.80];
-Y = [0.50, 0.60, 0.50, 0.40, 0.60, 0.50, 0.75, 0.85, 0.95];
-Z = [0.45, 0.55, 0.50, 0.60, 0.40, 0.52, 0.57, 0.63, 0.65];
+% X = [0.40, 0.60, 0.55, 0.60, 0.50, 0.70, 0.60, 0.90, 0.80];
+% Y = [0.50, 0.60, 0.50, 0.40, 0.60, 0.50, 0.75, 0.85, 0.95];
+% Z = [0.45, 0.55, 0.50, 0.60, 0.40, 0.52, 0.57, 0.63, 0.65];
 
 % Initial positions - 7 nodes (deleted two from 9 nodes) - 8 seconds
 % X = [1.6910359269619974 1.0671555014553136 ...
@@ -120,11 +124,6 @@ Z = [0.45, 0.55, 0.50, 0.60, 0.40, 0.52, 0.57, 0.63, 0.65];
 % X = [0.6];
 % Y = [0.1];
 % Z = [zmax-0.01];
-
-% All cases
-% X = [0.4, 0.7, 1.7, 1.8, 1.2, 1.7, 1.8];
-% Y = [0.5, 0.4, 0.5, 0.4, 1.5, 1.9, 1.4];
-% Z = [0.7, 0.7, 1.3, 0.6, 1.3, 1.0, 0.7];
 
 % Almost coincident nodes
 % X = [1, 1] + 0.2;
@@ -166,6 +165,10 @@ f = zeros(1, N);
 C = cell([1 N]);
 % Sensed space partitioning
 W = cell([1 N]);
+% Control inputs
+uX = zeros(1,N);
+uY = zeros(1,N);
+uZ = zeros(1,N);
 % Communication range for each node
 r_comm = zeros(1,N);
 % Adjacency matrix for communication graph
@@ -258,115 +261,15 @@ for s=1:smax
     
     
     % ----------------- Control law -----------------
-    move_vectors = zeros(2, N); % [uX ; uY]
-    uZ = zeros(1,N);
     for i=1:N
-       if ~isempty(W{i})
-            % Keep only CW (external) contours
-            % If Wi has holes, remove the corresponding contours since they
-            % dont contribute to the control law
-            % Find NaN indices
-            nanindex = find( isnan( W{i}(1,:) ) );
-            if ~isempty( nanindex )
-                Wi = []; % It will contain the external contour of Wi
-                indices = [ 0 nanindex length( W{i}(1,:) )+1 ];
-                for k=1:length(nanindex)+1
-                    % Keep a part of Wi
-                    tempx = W{i}(1, indices(k)+1 : indices(k+1)-1 );
-                    tempy = W{i}(2, indices(k)+1 : indices(k+1)-1 );
-                    if ispolycw(tempx, tempy)
-                        Wi = [Wi [tempx ; tempy]];
-                    end
-
-                end
-            else
-                % Wi has no holes
-                Wi = W{i};
-            end
-
-
-            % Integrate over the arcs
-            % Loop over all line segments of Wi
-            % Wi is used instead of W{i} to include cases with NaNs
-            % Wi is a closed list of points
-            for k=1:length(Wi(1,:))-1
-                % endpoints of the current line segment
-                pt1 = Wi(:,k);
-                pt2 = Wi(:,k+1);
-
-                % Check if they are on the boundary. If both are on it dont
-                % integrate
-                [~, onB1] = inpolygon( pt1(1), pt1(2), Xb, Yb );
-                [~, onB2] = inpolygon( pt2(1), pt2(2), Xb, Yb );
-
-                if ~(onB1 && onB2)
-                    % Check if any of them is on Ci, if not, dont integrate
-                    [~, onCi1] = inpolygon( pt1(1), pt1(2), C{i}(1,:), C{i}(2,:) );
-                    [~, onCi2] = inpolygon( pt2(1), pt2(2), C{i}(1,:), C{i}(2,:) );
-
-                    if (onCi1 || onCi2)
-                        % Check if they are both inside Cj for all j in overlap
-                        % If they are, then this is a dominant arc
-                        free_arc = 1; % Free arc flag
-                        % Loop over all overlapping nodes
-                        for j=1:N
-                            if A(i,j) && i~=j
-                                [inCj1] = inpolygon( pt1(1), pt1(2), C{j}(1,:), C{j}(2,:) );
-                                [inCj2] = inpolygon( pt2(1), pt2(2), C{j}(1,:), C{j}(2,:) );
-
-                                if inCj1 && inCj2
-                                    % This is a dominant arc, normal vector with
-                                    % magnitude fi-fj
-
-                                    d = norm( [pt1(1)-pt2(1) , pt1(2)-pt2(2)] );
-                                    n1 = (pt1-[X(i) ; Y(i)]) / R(i);
-                                    n2 = (pt2-[X(i) ; Y(i)]) / R(i);
-                                    nvector = (n1 + n2) / 2;
-
-                                    % X-Y control law
-                                    move_vectors(:,i) = move_vectors(:,i) + ...
-                                        (f(i)-f(j)) * d * nvector;
-
-                                    % Z control law
-                                    uZ(i) = uZ(i) + (f(i)-f(j))*tan(a)*d;
-                                end
-
-                                % If any of the points is inside a Cj, this is
-                                % not a free arc
-                                if inCj1 || inCj2
-                                    free_arc = 0;
-                                end
-                            end
-                        end % All other node for
-
-                        if free_arc
-                            % This is a free arc, normal vector
-                            d = norm( [pt1(1)-pt2(1) , pt1(2)-pt2(2)] );
-                            n1 = (pt1-[X(i) ; Y(i)]) / R(i);
-                            n2 = (pt2-[X(i) ; Y(i)]) / R(i);
-                            nvector = (n1 + n2) / 2;
-
-                            % X-Y control law
-                            move_vectors(:,i) = move_vectors(:,i) + ...
-                                f(i) * d * nvector;
-
-                            % Z control law
-                            uZ(i) = uZ(i) + f(i)*tan(a)*d;
-                        end
-
-                    end
-                end
-            end % line segment for
-
-            % Area integral for Z control law
-            uZ(i) = uZ(i) + dfu(Z(i), zmin, zmax) * ...
-                polyarea_nan( W{i}(1,:), W{i}(2,:) );
-        end
-    end % node for
+        % Give correct info based on adjacency matrix A
+        [uX(i), uY(i), uZ(i)] = ...
+            control_uniform(region, phi, zmin, zmax, W, C, f, i, X(i), Y(i), Z(i), a);
+    end
     
     % Control inputs
-    uX = axy * move_vectors(1,:);
-    uY = axy * move_vectors(2,:);
+    uX = axy * uX;
+    uY = axy * uY;
     uZ = az * uZ;
     
     

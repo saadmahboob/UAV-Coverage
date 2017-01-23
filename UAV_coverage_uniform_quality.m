61b3850d2a3b8e962125b8a0437cb6db8f14e976
sotpapathe <sotpapathe@google.com>
2017-01-23 16:41:42 +0100

    Fixed minor file naming bug

--- UAV_coverage_uniform_quality.m
+++ UAV_coverage_uniform_quality.m
@@ -35,7 +35,7 @@ a = 20*pi/180;
 % Simulation duration in seconds
 Tfinal = 10;
 % Time step in seconds
-Tstep = 0.1;
+Tstep = 0.01;
 
 % Control law options
 % Planar control law gain
@@ -44,13 +44,13 @@ axy = 1;
 az = 1;
 
 % Network plots to show during simulation
-PLOT_STATE_2D = 1;
+PLOT_STATE_2D = 0;
 PLOT_STATE_3D = 0;
 PLOT_STATE_QUALITY = 0;
 SAVE_PLOTS = 0;
 
 % Save simulation results to file
-SAVE_RESULTS = 0;
+SAVE_RESULTS = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -73,14 +73,14 @@ axis_scale = [-0.5 3 -0.5 3];
 
 % ---------------- Initial State ----------------
 % Initial positions - 3 nodes - 15 seconds
-X = [0.40, 0.60, 0.55];
-Y = [0.50, 0.60, 0.50];
-Z = [0.45, 0.55, 0.50];
+% X = [0.40, 0.60, 0.55];
+% Y = [0.50, 0.60, 0.50];
+% Z = [0.45, 0.55, 0.50];
 
 % Initial positions - 9 nodes - 20 seconds
-% X = [0.40, 0.60, 0.55, 0.60, 0.50, 0.70, 0.60, 0.90, 0.80];
-% Y = [0.50, 0.60, 0.50, 0.40, 0.60, 0.50, 0.75, 0.85, 0.95];
-% Z = [0.45, 0.55, 0.50, 0.60, 0.40, 0.52, 0.57, 0.63, 0.65];
+X = [0.40, 0.60, 0.55, 0.60, 0.50, 0.70, 0.60, 0.90, 0.80];
+Y = [0.50, 0.60, 0.50, 0.40, 0.60, 0.50, 0.75, 0.85, 0.95];
+Z = [0.45, 0.55, 0.50, 0.60, 0.40, 0.52, 0.57, 0.63, 0.65];
 
 % Initial positions - 7 nodes (deleted two from 9 nodes) - 8 seconds
 % X = [1.6910359269619974 1.0671555014553136 ...
@@ -268,7 +268,7 @@ for s=1:smax
         [uX(i), uY(i), uZ(i)] = ...
             control_uniform(region, zmin, zmax, a, ...
             W(logical(A(i,:))), C(logical(A(i,:))), f(logical(A(i,:))), ...
-            i, X(i), Y(i), Z(i));
+            ind, X(i), Y(i), Z(i));
     end
     
     % Control inputs with gain
